ea2de5c84f7ab36bfd8f79e1ef40d62e5b2a6264
Josue WSL2 <dev.carlos.morales@gmail.com>
2023-08-13 13:13:33 -0500

    feat(guards) add guards

--- src/app/app-routing.ts
+++ src/app/app-routing.ts
@@ -1,12 +1,16 @@
 import { Routes } from '@angular/router';
 
+import { authGuard, publicGuard } from './core/guards';
+
 export const routes: Routes = [
   {
     path: '',
+    canActivate: [authGuard],
     loadComponent: () => import('./pages/home/home.component'),
   },
   {
     path: 'auth',
+    canActivate: [publicGuard],
     children: [
       {
         path: 'sign-up',
---
+++ src/app/core/guards/auth.guard.ts
@@ -0,0 +1,37 @@
+import { inject } from '@angular/core';
+import { CanActivateFn, Router } from '@angular/router';
+import { AuthService } from '../services/auth.service';
+
+import { map } from 'rxjs';
+
+export const routerInjection = () => inject(Router);
+
+export const authStateObs$ = () => inject(AuthService).authState$;
+
+export const authGuard: CanActivateFn = () => {
+  const router = routerInjection();
+
+  return authStateObs$().pipe(
+    map((user) => {
+      if (!user) {
+        router.navigateByUrl('auth/log-in');
+        return false;
+      }
+      return true;
+    })
+  );
+};
+
+export const publicGuard: CanActivateFn = () => {
+  const router = routerInjection();
+
+  return authStateObs$().pipe(
+    map((user) => {
+      if (user) {
+        router.navigateByUrl('/');
+        return false;
+      }
+      return true;
+    })
+  );
+};
