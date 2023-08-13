c5f793ba4079965c5ee5db604d9f73e99faf422c
Josue WSL2 <dev.carlos.morales@gmail.com>
2023-08-13 12:39:20 -0500

    feat(service): auth service

---
+++ src/app/core/services/auth.service.ts
@@ -0,0 +1,42 @@
+import { Injectable, inject } from '@angular/core';
+import {
+  Auth,
+  UserCredential,
+  authState,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from '@angular/fire/auth';
+
+export interface Credential {
+  email: string;
+  password: string;
+}
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  private auth: Auth = inject(Auth);
+
+  readonly authState$ = authState(this.auth);
+
+  signUpWithEmailAndPassword(credential: Credential): Promise<UserCredential> {
+    return createUserWithEmailAndPassword(
+      this.auth,
+      credential.email,
+      credential.password
+    );
+  }
+
+  logInWithEmailAndPassword(credential: Credential) {
+    return signInWithEmailAndPassword(
+      this.auth,
+      credential.email,
+      credential.password
+    );
+  }
+
+  logOut(): Promise<void> {
+    return this.auth.signOut();
+  }
+}
