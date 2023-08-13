9b348b2643d24ba59c942be12abf53a60b32d022
Josue WSL2 <dev.carlos.morales@gmail.com>
2023-08-13 11:48:33 -0500

    feat(config) browserAnimation module

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,23 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { firebaseProviders } from './firebase.config';
 import { routes } from './app-routing';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MAT_FORM_FIELD_DEFAULT_OPTIONS } from '@angular/material/form-field';
+
+const NO_NG_MODULES = importProvidersFrom([BrowserAnimationsModule]);
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), firebaseProviders],
+  providers: [
+    provideRouter(routes),
+    firebaseProviders,
+    NO_NG_MODULES,
+    {
+      provide: MAT_FORM_FIELD_DEFAULT_OPTIONS,
+      useValue: {
+        appearance: 'outline',
+        color: 'accent',
+      },
+    },
+  ],
 };
