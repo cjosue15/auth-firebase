f0a9160f2ccbeccae51348d81315e72271c21224
Josue WSL2 <dev.carlos.morales@gmail.com>
2023-08-13 11:47:50 -0500

    feat(log-in) create page for auth

---
+++ src/app/pages/auth/log-in/log-in.component.ts
@@ -0,0 +1,55 @@
+import { Component, inject } from '@angular/core';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { RouterModule } from '@angular/router';
+
+interface LogInForm {
+  email: FormControl<string>;
+  password: FormControl<string>;
+}
+
+@Component({
+  standalone: true,
+  imports: [
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
+    MatButtonModule,
+    ReactiveFormsModule,
+    RouterModule,
+  ],
+  selector: 'app-log-in',
+  templateUrl: './log-in.component.html',
+  providers: [],
+})
+export default class LogInComponent {
+  hide = true;
+
+  formBuilder = inject(FormBuilder);
+
+  form: FormGroup<LogInForm> = this.formBuilder.group({
+    email: this.formBuilder.control('', {
+      validators: [Validators.required, Validators.email],
+      nonNullable: true,
+    }),
+    password: this.formBuilder.control('', {
+      validators: Validators.required,
+      nonNullable: true,
+    }),
+  });
+
+  logIn(): void {
+    if (this.form.invalid) return;
+    console.log(this.form.value);
+  }
+}
